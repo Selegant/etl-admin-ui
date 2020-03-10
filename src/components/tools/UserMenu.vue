90d279ad4d1dc2d14f51efe4ad48e434d36841b1
selegant <1228579928@qq.com>
2020-03-10 17:43:49 +0800

    去除 消息通知和个人设置

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="user-wrapper">
     <div class="content-box">
-      <a href="https://pro.loacg.com/docs/getting-started" target="_blank">
-        <span class="action">
-          <a-icon type="question-circle-o"></a-icon>
-        </span>
-      </a>
-      <notice-icon class="action"/>
+      <!--      去除消息通知-->
+      <!--      <a href="https://pro.loacg.com/docs/getting-started" target="_blank">-->
+      <!--        <span class="action">-->
+      <!--          <a-icon type="question-circle-o"></a-icon>-->
+      <!--        </span>-->
+      <!--      </a>-->
+      <!--      <notice-icon class="action"/>-->
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
           <a-avatar class="avatar" size="small" :src="avatar"/>
           <span>{{ nickname }}</span>
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
-          <a-menu-item key="0">
-            <router-link :to="{ name: 'center' }">
-              <a-icon type="user"/>
-              <span>个人中心</span>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item key="1">
-            <router-link :to="{ name: 'settings' }">
-              <a-icon type="setting"/>
-              <span>账户设置</span>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item key="2" disabled>
-            <a-icon type="setting"/>
-            <span>测试</span>
-          </a-menu-item>
-          <a-menu-divider/>
+          <!--          <a-menu-item key="0">-->
+          <!--            <router-link :to="{ name: 'center' }">-->
+          <!--              <a-icon type="user"/>-->
+          <!--              <span>个人中心</span>-->
+          <!--            </router-link>-->
+          <!--          </a-menu-item>-->
+          <!--          <a-menu-item key="1">-->
+          <!--            <router-link :to="{ name: 'settings' }">-->
+          <!--              <a-icon type="setting"/>-->
+          <!--              <span>账户设置</span>-->
+          <!--            </router-link>-->
+          <!--          </a-menu-item>-->
+          <!--          <a-menu-item key="2" disabled>-->
+          <!--            <a-icon type="setting"/>-->
+          <!--            <span>测试</span>-->
+          <!--          </a-menu-item>-->
+          <!--          <a-menu-divider/>-->
           <a-menu-item key="3">
             <a href="javascript:;" @click="handleLogout">
               <a-icon type="logout"/>
